2404649a91bcb3a5ed20696caf7ef75954bff2f7
Hippeius <jnaputi509@gmail.com>
2016-08-09 20:34:39 -0700

    DB is now operational

--- DotNetProgrammingFinal/Assets/Scripts/TriviaQA.cs
+++ DotNetProgrammingFinal/Assets/Scripts/TriviaQA.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-
+using System;
+using Mono.Data.Sqlite;
 
 public class TriviaQA {
 
@@ -31,7 +32,31 @@ public class TriviaQA {
 
     public void GetTriviaQuestions()
     {
-       
+		string connectionString = "URI=file:" + Application.dataPath + "/TRIVIA_MAZE.sqlite";
+
+		using (SqliteConnection connection = new SqliteConnection (connectionString)) {
+			connection.Open ();
+
+			using (SqliteCommand command = connection.CreateCommand ()) {
+				command.CommandText = "SELECT * FROM QUESTIONS WHERE ID=105";
+
+				using (SqliteDataReader reader = command.ExecuteReader ()) {
+					while (reader.Read ()) {
+						Debug.Log ("ID: " + reader ["ID"]);
+						Debug.Log ("First Question: " + reader ["QUESTION"]);
+						Debug.Log ("First Answer: " + reader ["ANS_A"]);
+						Debug.Log ("Second Answer: " + reader ["ANS_B"]);
+						Debug.Log ("Third Answer: " + reader ["ANS_C"]);
+						Debug.Log ("Fourth Answer: " + reader ["ANS_D"]);
+						Debug.Log ("Actual Answer: " + reader ["ANSWER"]);
+					}
+
+					reader.Close ();
+				}
+			}
+
+			connection.Close ();
+		}
     }
 
     public class TriviaQuestion
